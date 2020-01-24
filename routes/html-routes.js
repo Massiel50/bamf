b32f0d32971089d50ec079a43f362c7b1c5f9799
Juan De La Guardia <delaguardia@outlook.com>
2020-01-24 12:30:03 -0500

    working fourth

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,12 +6,13 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
-  app.get("/home", function(req, res) {
+  app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("home")
   });
 
   app.get("/login", function(req, res) {
