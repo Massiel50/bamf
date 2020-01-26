873cef366f17614d32b383b53206c49ea46b57de
Avery Stahl <averystahl@hotmail.com>
2020-01-26 18:03:16 -0500

    omdb api call

---
+++ controllers/moviescontrollers.js
@@ -0,0 +1,15 @@
+// Dependencies
+const axios = require("axios");
+
+// User Movie request
+const movie;
+// Get request from OMDB API
+axios.get(`https://www.omdbapi.com/?t=${movie}&apikey=trilogy`)
+    .then(function(res)
+    {
+        // Title
+        console.log(res.data.imdbID);
+        console.log(res.data.Poster);
+        console.log(res.data.Title);
+        console.log(res.data.Plot);
+    });
