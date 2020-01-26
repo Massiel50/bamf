119fc501dabf40fc0fec7180bc854d08048c54e5
Juan De La Guardia <delaguardia@outlook.com>
2020-01-26 17:45:27 -0500

    axios calland logout folder

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,7 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
 var passport = require("../config/passport");
-
+var axios = require("axios");
 module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
@@ -48,4 +48,15 @@ module.exports = function(app) {
       });
     }
   });
+  // app.get("/moviedata", function (req,res){
+  //   axios.get("https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy")
+
+    
+
+
+  //   .then(function(data){
+  //     console.log(data);
+      
+  //   })
+  // })
 };
