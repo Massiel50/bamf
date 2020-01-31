74390aeeb99691dd7c5f9e00ecdd718aac229e16
Massiel50 <massielescano@yahoo.com>
2020-01-31 10:38:53 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,2 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  // host: "localhost",
-  // port: 3306,
-  // user: "root",
-  // password: "",
-  // database: "bamfmovies_db"
-  "use_env_variables": "JAWSDB_URL",
-  "dialect": "mysql"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
 
