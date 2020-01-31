0be8d022fe727b817fd81802ac1a6d0a23656762
Massiel50 <massielescano@yahoo.com>
2020-01-31 10:15:03 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
 // var connection = mysql.createConnection({
 //   host: "localhost",
@@ -10,14 +10,14 @@ var mysql = require("mysql");
 // });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
-module.exports = connection;
+// module.exports = connection;
 
